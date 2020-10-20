7e3290e44c9720935e5a7e7d9917f0a0179df189
ieyaolinan <ieyaolinan@google.com>
2020-10-21 01:05:53 +0800

    正在学习运算符

---
+++ HelloWorld/src/Display.java
@@ -0,0 +1,14 @@
+public class Display {
+    private static void print(String[] args) {
+        System.out.println(args[1]);
+    }
+
+    public static void printf(String[] args) {
+        System.out.println(args[1]);
+    }
+
+    public static void dis(Object args) {
+        System.out.println(args);
+    }
+
+}
